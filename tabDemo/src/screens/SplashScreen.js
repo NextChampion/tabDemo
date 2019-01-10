71588ad5d296cdfcce5500aecaea977279a7d161
zhangcunxia <252946169@qq.com>
2019-01-10 22:53:20 +0800

    显示倒计时

--- tabDemo/src/screens/SplashScreen.js
+++ tabDemo/src/screens/SplashScreen.js
@@ -12,18 +12,38 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 type Props = {};
 export default class SplashScreen extends Component<Props> {
 
+  state = {
+    number: 3,
+  }
+
+  setInterval = {};
+  stateAvaliable = true;
+
   componentDidMount() {
+    this.setInterval = setInterval(()=>{
+      const { number } = this.state;
+      const nextNumberState = number - 1;
+      if (nextNumberState > 0 && this.stateAvaliable) {
+        this.setState({number: nextNumberState});
+      }
+    }, 1000);
     setTimeout(()=>{
       const { navigation } = this.props;
       navigation.navigate('main');
-    })
+    },3000);
   }
 
+  componentWillUnount() {
+    this.stateAvaliable = false;
+    clearInterval(this.setInterval);
+  }
 
   render() {
+    const { number } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.instructions}>SplashScreen</Text>
+        <Text style={styles.instructions}>{number}</Text>
       </View>
     );
   }
